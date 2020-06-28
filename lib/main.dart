863ec7b42a0c7ee5025dec5f56769f9a32ff2be2
HERMES EMMANUEL DOS PINHEIRO <hermesprojetos30@gmail.com>
2020-06-27 21:12:02 -0300

    entrega de trabalho

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[700],
+        appBar: AppBar(
+          title: Text('Visualizador de Imagens'),
+          backgroundColor: Colors.green[500],
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: NetworkImage(
+                          'https://icon-icons.com/icons2/97/PNG/256/brazil_flags_flag_16979.png'),
+                    ),
+                    Image(
+                      image: AssetImage('images/canada.png'),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('images/inglaterra.png'),
+                    ),
+                    Image(
+                      image: NetworkImage(
+                          'https://cdn.icon-icons.com/icons2/97/PNG/256/belgium_flags_flag_16976.png'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
