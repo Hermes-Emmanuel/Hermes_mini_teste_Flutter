5ed17383417d3c001708c98a04fa8404e8769df6
HERMES EMMANUEL DOS PINHEIRO <hermesprojetos30@gmail.com>
2020-06-27 21:35:34 -0300

    correção da banner de debug

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey[700],
         appBar: AppBar(
